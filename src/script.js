5f019639be840878f8486e3fd6b374aaada3aa9f
Julia Stetsiura <stetsiura.iuliia@gmail.com>
2023-06-22 23:50:15 +0300

    committed

---
+++ src/script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('myButton');
+    const nameElement = document.getElementById('name');
+    const ageElement = document.getElementById('age');
+    const emailElement = document.getElementById('email');
+    const addressElement = document.getElementById('address');
+    const positionElement = document.getElementById('position');
+    const skillsElement = document.getElementById('skills');
+    const tableElement = document.getElementById('data-table');
+  
+    button.addEventListener('click', function() {
+      const name = 'Julia';
+      const age = 31;
+      const email = 'stetsiura.iuliia@gmail.com';
+      const country = 'Best in the World 🇺🇦';
+      const city = 'Kyiv';
+      const street = 'Chokolivskyi blvd.';
+      const position = 'Product Manager';
+      const skills = 'SDLC, UI/UX, Agile';
+  
+      const address = `${country}, ${city}, ${street}`;
+  
+      nameElement.textContent = name;
+      ageElement.textContent = age;
+      emailElement.innerHTML = '<a href="mailto:' + email + '">' + email + '</a>';
+      addressElement.textContent = address;
+      positionElement.textContent = position;
+      skillsElement.textContent = skills;
+  
+      tableElement.style.display = 'table';
+    });
+  });
+  
